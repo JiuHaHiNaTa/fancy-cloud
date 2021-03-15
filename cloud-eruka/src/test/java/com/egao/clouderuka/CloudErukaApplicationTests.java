d0039617318e96fba5ccf88ef7698626cc1f6518
jiuha <jiuhamashiro@sina.com>
2021-03-15 11:18:49 +0800

    配置服务器搭建与测试，配置服务器相关git和本地文件系统测试

---
+++ cloud-eruka/src/test/java/com/egao/clouderuka/CloudErukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egao.clouderuka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudErukaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
