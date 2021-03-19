d6fef3d9e91da5fdc0b2cc23e9d478917b11c9a9
jiuha <jiuhamashiro@sina.com>
2021-03-19 17:51:50 +0800

    eureka服务器搭建，测试服务调用

--- cloud-demo/src/main/java/com/egao/clouddemo/controller/HelloController.java
+++ cloud-demo/src/main/java/com/egao/clouddemo/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.egao.clouddemo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
